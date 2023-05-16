c8163916bab6f7442a0d99bf08c43cd76fc1ffd0
Guilherme Pereira <guilherme.mp.eng@gmail.com>
2023-05-16 19:28:27 -0300

    Add 'Feed' and 'Post' models as well new migration file

---
+++ app/models/feed.py
@@ -0,0 +1,17 @@
+import uuid
+
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy.dialects.postgresql import UUID
+
+from app.db.base_class import Base
+
+
+class Feed(Base):
+    id = Column(Integer, autoincrement=True, primary_key=True, index=True)
+    uuid = Column(UUID(as_uuid=True), unique=True, index=True, default=uuid.uuid4())
+    title = Column(String(255), unique=True, nullable=False, index=True)
+    description = Column(String(1000), nullable=False)
+    link = Column(String(255), unique=True, nullable=False)
+    language = Column(String(255), nullable=True)
+    last_build_date = Column(DateTime, nullable=True, index=True)
+    refresh_enabled = Column(Boolean(), nullable=False, default=True)
---
+++ app/models/post.py
@@ -0,0 +1,16 @@
+import uuid
+
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.dialects.postgresql import UUID
+
+from app.db.base_class import Base
+
+
+class Post(Base):
+    id = Column(Integer, autoincrement=True, primary_key=True, index=True)
+    feed_id = Column(Integer, ForeignKey("feed.id"))
+    uuid = Column(UUID(as_uuid=True), unique=True, index=True, default=uuid.uuid4())
+    title = Column(String(255), unique=True, nullable=False, index=True)
+    description = Column(String(1000), nullable=False)
+    link = Column(String(255), unique=True, nullable=False)
+    pub_date = Column(DateTime(), nullable=False, index=True)
