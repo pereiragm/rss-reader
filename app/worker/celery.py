28deb79d41001015949351729a32ef7bc343fa87
Guilherme Pereira <guilherme.mp.eng@gmail.com>
2023-05-20 15:01:50 -0300

    Add scheduler config (beat) to trigger 'refresh_feeds_routine' every 5 minutes

--- app/worker/celery.py
+++ app/worker/celery.py
@@ -9,3 +9,11 @@ app = Celery(
         "app.worker.tasks",
     ],
 )
+
+app.conf.beat_schedule = {
+    'refresh-feeds-routine': {
+        'task': 'app.worker.tasks.refresh_feeds_routine',
+        'schedule': 300,  # every 5 minutes
+    },
+}
+app.conf.timezone = 'UTC'
