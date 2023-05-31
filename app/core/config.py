5e6431e6efd6108b2aa4580596de640a986dbe46
Guilherme Pereira <guilherme.mp.eng@gmail.com>
2023-05-31 11:15:05 -0300

    Fix config to make broker work with docker compose

--- app/core/config.py
+++ app/core/config.py
@@ -3,16 +3,9 @@ from pydantic.env_settings import BaseSettings
 
 class Settings(BaseSettings):
     ENVIRONMENT: str
-
-    # Postgres
-    # POSTGRES_USER: str
-    # POSTGRES_PASSWORD: str
-    # POSTGRES_HOST: str
-    # POSTGRES_PORT: str
-    # SQLALCHEMY_DATABASE_URL: str = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/rss_reader"  # noqa
     SQLALCHEMY_DATABASE_URL: str
-    # TEST_SQLALCHEMY_DATABASE_URL: str = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/rss_reader_test"
     TEST_SQLALCHEMY_DATABASE_URL: str
+    BROKER_URL: str
 
     class Config:
         case_sensitive = True
